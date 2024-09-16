42090a8da6144c0707e0f3db5e1fea0e7b728e96
TorisutanKholwes <tristanclowez@icloud.com>
2024-09-17 01:18:30 +0200

    Almost finish

--- src/components/pokedle/Pokedle.js
+++ src/components/pokedle/Pokedle.js
@@ -1,11 +1,47 @@
 import React from "react";
+import ReactDom from "react-dom/client";
 
 import PokedleAnswer from "./PokedleAnswer";
 
 import "./Pokedle.scss"
 
 export default class Pokedle extends React.Component {
-    test = (e) => {
+
+    listAnswer = [];
+    answerRoot;
+
+    constructor(props) {
+        super(props);
+        this.randomPokemonId = Math.floor(Math.random() * 1025) + 1;
+        this.state = {
+            data: [],
+            listData: []
+        }
+    }
+
+    componentDidMount() {
+        this.fetchData();
+        this.answerRoot = ReactDom.createRoot(document.getElementById("poke-answer"));
+    }
+
+    fetchData = async () => {
+        const allPokemonResponse = await fetch(`https://tyradex.vercel.app/api/v1/pokemon`)
+        const allPokemonData = await allPokemonResponse.json()
+        let filtered = allPokemonData.filter(pokemon => this.getActiveGeneration().includes(pokemon.generation));
+        let randomIndex = Math.floor(Math.random() * filtered.length);
+        let randomPokemon = filtered[randomIndex].pokedex_id;
+        this.randomPokemonId = randomPokemon;
+        const response = await fetch(`https://tyradex.vercel.app/api/v1/pokemon/${this.randomPokemonId}`)
+        const data = await response.json()
+        this.setState({data: data})
+        const responseList = await fetch(`https://tyradex.vercel.app/api/v1/pokemon`)
+        const dataList = await responseList.json()
+        this.setState({listData: dataList})
+        return randomPokemon;
+    }
+
+    activeGeneration = (e) => {
+        if (this.getActiveGeneration().length === 1 && e.target.classList.contains("active")) return;
         if (e.target.classList.contains("active")) {
             e.target.classList.remove("active");
         } else {
@@ -13,25 +49,153 @@ export default class Pokedle extends React.Component {
         }
     }
 
+    getActiveGeneration = () => {
+        let gens = document.getElementsByClassName("starter");
+        let actives = [];
+        for (let i = 0; i < gens.length; i++) {
+            if (gens[i].classList.contains("active")) {
+                actives.push(i + 1);
+            }
+        }
+        return actives;
+    }
+
+    regenerate = () => {
+        this.fetchData();
+        let answer = document.getElementById("poke-answer");
+        answer.innerHTML = "";
+        setTimeout(() => {
+            console.log("New : " + this.randomPokemonId);
+        }, 200)
+    }
+
+    showPokemon = () => {
+        let input = document.getElementById("pokemon-list");
+        input.style.display = "block";
+
+    }
+
+    hidePokemon = () => {
+        setTimeout(() => {
+            let input = document.getElementById("pokemon-list");
+            input.style.display = "none";
+        }, 150);
+    }
+
+    updatePokemon = (e) => {
+        let input = e.target.value;
+        let pokemons = document.getElementsByClassName("pokemon-list-element");
+        let showedPokemon = 0;
+        for (let i = 0; i < pokemons.length; i++) {
+            let pokemon = pokemons[i];
+            if (pokemon.textContent.toLowerCase().startsWith(input.toLowerCase())) {
+                pokemon.style.display = "flex";
+                showedPokemon++;
+            } else {
+                pokemon.style.display = "none";
+            }
+        }
+        let list = document.getElementById("pokemon-list");
+        if (showedPokemon < 4) {
+            list.style.height = "auto";
+        } else if (showedPokemon >= 4 && list.style.height === "auto") {
+            list.style.height = "400px";
+        }
+    }
+
+    checkKey = (e) => {
+        if (e.key !== "Enter") return;
+        let input = document.getElementById("poke-input");
+        let value = input.value;
+        let {listData} = this.state;
+        let pokemon = listData.find(pokemon => pokemon.name.fr.toLowerCase() === value.toLowerCase() && this.getActiveGeneration().includes(pokemon.generation));
+        if (pokemon) {
+            this.validateAnswer(pokemon.pokedex_id);
+        }
+    }
+
+    checkClick = (e) => {
+        let target = e.target;
+        if (target.tagName === "IMG" || target.tagName === "P") {
+            target = target.parentElement;
+        }
+        let value = target.textContent;
+        let {listData} = this.state;
+        let pokemon = listData.find(pokemon => pokemon.name.fr.toLowerCase() === value.toLowerCase());
+        if (pokemon) {
+            this.validateAnswer(pokemon.pokedex_id);
+        }
+    }
+
+    validateAnswer = (id) => {
+        let input = document.getElementById("poke-input");
+        input.value = "";
+        input.blur();
+        let pokemons = document.getElementsByClassName("pokemon-list-element");
+        for (let i = 0; i < pokemons.length; i++) {
+            pokemons[i].style.display = "flex";
+        }
+        document.getElementById("pokemon-list").style.height = "400px";
+        this.hidePokemon();
+
+        this.listAnswer.push(<PokedleAnswer key={this.listAnswer.length+1} pokemonId={id} goodAnswerId={this.randomPokemonId}/>);
+        this.answerRoot.unmount();
+        this.answerRoot = ReactDom.createRoot(document.getElementById("poke-answer"));
+        console.log(this.listAnswer);
+        this.answerRoot.render(<> {this.listAnswer.reverse()} </>);
+        setTimeout(() => {
+            this.listAnswer.reverse()
+        }, 150)
+    }
+
     render() {
+        let {listData} = this.state;
         let startersIDs = [909, 813, 725, 653, 498, 390, 255, 155, 4].reverse()
         let gens = [];
         for (let i = 0; i < 9; i++) {
-            gens.push(<img onClick={this.test} key={`key-${i}`} className="starter active" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${startersIDs[i]}.png`} alt={`starter-${startersIDs[i]}`}/>)
+            gens.push(<img onClick={this.activeGeneration} key={`key-${i}`} className="starter active" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${startersIDs[i]}.png`} alt={`starter-${startersIDs[i]}`}/>)
         }
         return (
         <div className="pokedle">
-            <h1>Pokedle</h1>
+            <h1 id="title">Pokedle</h1>
             <div id="generation">
                 <div id="genlist">{gens}</div>
-                <button id="valid-button">Valider</button>
+                <button id="valid-button" onClick={this.regenerate}>Valider</button>
             </div>
             <div id="input-div">
-                <input type="text"/>
-                <div id="autocomplete-pokemon"/>
+                <input id="poke-input" onFocus={this.showPokemon} onBlur={this.hidePokemon} onInput={this.updatePokemon} onKeyDown={this.checkKey} type="text"/>
+                <div id="pokemon-list">
+                    {listData.map(pokemon => {
+                        if (pokemon.pokedex_id === 0) return;
+                        if (this.getActiveGeneration().includes(pokemon.generation)) {
+                            //return <div key={"poke-" + pokemon.pokedex_id}> {pokemon.name ? pokemon.name.fr : ""} </div>
+                            return <div onClick={this.checkClick} key={"poke-" + pokemon.pokedex_id} className="pokemon-list-element">
+                                <img
+                                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.pokedex_id}.png`}
+                                    alt=""/>
+                                <p>{pokemon.name.fr}</p>
+                            </div>
+                        }
+                    })}
+                </div>
             </div>
-            <PokedleAnswer pokemonId={6} goodAnswerId={255}/>
-            <PokedleAnswer pokemonId={383} goodAnswerId={255}/>
+            <div id="category">
+                <p>Nom</p>
+                <p>Photo</p>
+                <p>Type 1</p>
+                <p>Type 2</p>
+                <p>Stade évolution</p>
+                <p>Entièrement évolué</p>
+                <p>Taux de capture</p>
+                <p>Génération</p>
+            </div>
+            <div id="poke-answer">
+                <PokedleAnswer pokemonId={6} goodAnswerId={this.randomPokemonId}/>
+                <PokedleAnswer pokemonId={383} goodAnswerId={this.randomPokemonId}/>
+                <PokedleAnswer pokemonId={364} goodAnswerId={this.randomPokemonId}/>
+                <PokedleAnswer pokemonId={229} goodAnswerId={this.randomPokemonId}/>
+                <PokedleAnswer pokemonId={this.randomPokemonId} goodAnswerId={this.randomPokemonId}/>
+            </div>
         </div>
         );
     }
--- src/components/pokedle/PokedleAnswer.js
+++ src/components/pokedle/PokedleAnswer.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import arrow from "../../resources/arrow.png"
+
 import "./PokedleAnswer.scss"
 
 export default class PokedleAnswer extends React.Component {
@@ -7,7 +9,8 @@ export default class PokedleAnswer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            goodAnswerData: []
         }
         this.pokemonId = props.pokemonId;
         this.goodAnswerId = props.goodAnswerId;
@@ -16,39 +19,66 @@ export default class PokedleAnswer extends React.Component {
 
     componentDidMount() {
         this.fetchData();
-        /*let answerContent = [<div className="pokemon-image"><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonId}.png`} alt=""/></div>]
-        fetch(`https://tyradex.vercel.app/api/v1/pokemon/${this.pokemonId}`).then(res => res.json())
-            .then(data => {
-                answerContent.push(<div className="poke-type1">`${data.types[0].name}`</div>)
-            })*/
     }
 
     fetchData = async () => {
-        const response = await fetch(`https://tyradex.vercel.app/api/v1/pokemon/${this.pokemonId}`)
-        const data = await response.json()
-        this.setState({data: data})
+        const responseInput = await fetch(`https://tyradex.vercel.app/api/v1/pokemon/${this.pokemonId}`)
+        const dataInput = await responseInput.json()
+        const responseGoodAnswer = await fetch(`https://tyradex.vercel.app/api/v1/pokemon/${this.goodAnswerId}`)
+        const dataGoodAnswer = await responseGoodAnswer.json()
+        this.setState({data: dataInput, goodAnswerData: dataGoodAnswer})
     }
 
     render() {
-        const {data } = this.state;
-        console.log(data.evolution && data.evolution.pre ? "oui" : "non" )
+        const random = Math.floor(Math.random() * 50) + 1;
+        let imageUrl = (random === 1) ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${this.pokemonId}.png` : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonId}.png`;
+        const { data, goodAnswerData } = this.state;
+        let type1 = data.types && goodAnswerData.types ? data.types[0].name === goodAnswerData.types[0].name ? "correct" : goodAnswerData.types.length === 2 && goodAnswerData.types[1].name === data.types[0].name ? "mid" : "wrong" : "wrong";
+        let type2 = data.types && goodAnswerData.types ? (data.types.length === 1 && goodAnswerData.types.length === 1) || ((data.types.length === 2 && goodAnswerData.types.length === 2) && data.types[1].name === goodAnswerData.types[1].name) ? "correct" : data.types.length === 2 && data.types[1].name === goodAnswerData.types[0].name ? "mid" : "wrong" : "wrong";
+
+        // Evolution stade
+        let dataStade = data.evolution ? data.evolution.pre ? data.evolution.pre.length+1 : 1 : 1;
+        let goodAnswerStade = goodAnswerData.evolution ? goodAnswerData.evolution.pre ? goodAnswerData.evolution.pre.length+1 : 1 : 1;
+        let evolutionStade = dataStade === goodAnswerStade ? "correct" : "wrong";
+        let evolutionStadeDiff = (evolutionStade === "wrong") ? (dataStade - goodAnswerStade > 0) ? "less" : "more" : "";
+
+        // End evolution
+        let isEndEvolution = data.evolution ? data.evolution.next ? "Non" : "Oui" : "Oui";
+        let goodAnswerIsEndEvolution = goodAnswerData.evolution ? goodAnswerData.evolution.next ? "Non" : "Oui" : "Oui";
+        let endEvolution = isEndEvolution === goodAnswerIsEndEvolution ? "correct" : "wrong";
+
+        let catchRate = data.catch_rate && goodAnswerData.catch_rate ? data.catch_rate === goodAnswerData.catch_rate ? "correct" : "wrong" : "wrong";
+        let catchRateDiff = (catchRate === "wrong") ? (data.catch_rate && goodAnswerData.catch_rate ? data.catch_rate - goodAnswerData.catch_rate > 0 ? "less" : "more" : "") : ""
+
+        let gen = data.generation && goodAnswerData.generation ? data.generation === goodAnswerData.generation ? "correct" : "wrong" : "wrong";
+        let genDiff = (gen === "wrong") ? (data.generation && goodAnswerData.generation ? data.generation - goodAnswerData.generation > 0 ? "less" : "more" : "") : ""
+
         return <div className="pokemon-answer">
-            <div className="pokemon-image">
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonId}.png`} alt=""/></div>
-                <p>{data.name ? data.name.fr : ""}</p>
-            <div className="poke-type1">
+            <div className="poke-name">{data.name ? data.name.fr : ""}</div>
+            <div className="poke-image">
+                <img src={`${imageUrl}`} alt=""/>
+            </div>
+            <div className={`poke-type1 ${type1}`}>
                 {data.types ? data.types[0].name : ""}
             </div>
-            <div className="poke-type2">
+            <div className={`poke-type2 ${type2}`}>
                 {data.types && data.types.length === 2 ? data.types[1].name : "Aucun"}
             </div>
-            <div className="poke-evolution-stade">
-                {data.evolution ? data.evolution.pre ? data.evolution.pre.length : "1" : "1"}
+            <div className={`poke-evolution-stade ${evolutionStade} ${evolutionStadeDiff}`}>
+                {data.evolution ? data.evolution.pre ? data.evolution.pre.length+1 : "1" : "1"}
+                {evolutionStadeDiff !== "" ? <img className={`arrow ${evolutionStadeDiff}`} src={arrow} alt={"Arrow"} /> : "" }
             </div>
-            <div className="poke-is-end-evolution"/>
-            {data.evolution ? data.evolution.next ? "Non" : "Oui" : "Oui"}
-            <div className="poke-color"/>
-            <div className="poke-generation"/>
+            <div className={`poke-is-end-evolution ${endEvolution}`}>
+                {data.evolution ? data.evolution.next ? "Non" : "Oui" : "Oui"}
+            </div>
+            <div className={`poke-catchrate ${catchRate} ${catchRateDiff}`}>
+                {data.catch_rate ? data.catch_rate : ""}
+                {catchRateDiff !== "" ? <img className={`arrow ${catchRateDiff}`} src={arrow} alt={"Arrow"} /> : "" }
+            </div>
+            <div className={`poke-generation ${gen} ${genDiff}`}>
+                {data.generation ? data.generation : ""}
+                {genDiff !== "" ? <img className={`arrow ${genDiff}`} src={arrow} alt={"Arrow"} /> : "" }
+            </div>
         </div>
     }
 }
